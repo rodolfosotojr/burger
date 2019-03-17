9f91271512922823a154a44781665913f3ae2a51
Rodolfo <jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.com>
2019-03-17 16:22:34 -0500

    additional push

--- config/orm.js
+++ config/orm.js
@@ -1,36 +1,16 @@
+//A lot of this file was referenced from the MvcExample from handlebars ./14.3/16
+
 var connection = require("../config/connection");
 
 function getQuestionMarks(arr) {
     var questionMark = [];
-  
-    for (var i = 0; i < arr.length; i++) {
-      questionMark.push("?");
-    }
-  
-    return questionMark.join(", ");
-  }
 
-  function objToSql(ob) {
-    var arr = [];
-  
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
-        arr.push(key + "=" + value);
-      }
+    for (var i = 0; i < arr.length; i++) {
+        questionMark.push("?");
     }
-  
-    // translate array of strings to a single comma-separated string
-    return arr.join(", ");
-  }
+
+    return questionMark.join(", ");
+}
 
 var orm = {
     selectAll: function (tableInput, cb) {
@@ -44,21 +24,39 @@ var orm = {
     },
     insertOne: function (table, column, value, cb) {
         var query = "INSERT INTO " + table + "(" + column.join(", ") + ")" + "VALUES (" + getQuestionMarks(value) + ") ";
-        
+
         console.log(query);
 
-        connection.query(query, value, function(err, result){
+        connection.query(query, value, function (err, result) {
             if (err) {
                 throw err;
             }
             cb(result);
         });
     },
-    updateOne: function(table, objColVals, condition, cb){
-        var query = "UPDATE " + table " SET "
+    updateOne: function (table, assgn, condition, cb) {
+        var query = "UPDATE " + table + " SET " + getQuestionMarks(Object.keys(assgn)) + " WHERE " + condition;
+
+        connection.query(query, assgn, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     }
 };
 
-orm.insertOne("burgers", ["burger_name", "devoured"], ["Double Cheeseburger", false], function(res){
-    console.log(res);
-});
+// orm.selectAll("burgers", function(res){
+//     console.log(res);
+// });
+
+// orm.insertOne("burgers", ["burger_name", "devoured"], ["Double Cheeseburger", false], function(res){
+//     console.log(res);
+// });
+
+// orm.updateOne("burgers", [{ burger_name: "Double Cheeseburger" }, { devoured: true }], "id=1", function (res) {
+//     console.log(res);
+// });
+
+
